da7f5a202078c60d3af2092038aa51ad7273c3d6
Kevin Boyd <kevin.boyd@gmail.com>
2025-11-06 22:04:45 -0800

    Rector rules

--- rector.php
+++ rector.php
@@ -10,7 +10,7 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-    ->withPhpSets(php72: true)
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withPhpSets(php81: true)
+    ->withTypeCoverageLevel(70)
+    ->withDeadCodeLevel(70)
+    ->withCodeQualityLevel(70);
